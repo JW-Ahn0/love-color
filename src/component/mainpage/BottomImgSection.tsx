1a85724634e43c70d304ad07255d1d03073fd236
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-11-12 12:19:13 +0900

    refactor: img alt 변경

--- src/component/mainpage/BottomImgSection.tsx
+++ src/component/mainpage/BottomImgSection.tsx
@@ -1,58 +1,68 @@
 import styled from "@emotion/styled";
 import { IMAGES_PATH } from "../../config/mainapge/paths";
 interface BottomImgSectionProps {
-    isMobile: boolean;
+  isMobile: boolean;
 }
-const BottomImgSection:React.FC<BottomImgSectionProps> = ({isMobile}) => {
-    return(
-        <BottomImgSectionWrapper>
-            <img src={IMAGES_PATH.leftBottom}  alt="소현이 생일 축하 라년큘러스2" className="leftImg"></img>
-            {!isMobile && <img src={IMAGES_PATH.rightBottom} alt="소현이 생일 축하 목련4" className="rightImg"></img> }      
-        </BottomImgSectionWrapper>
-    )
-}
+const BottomImgSection: React.FC<BottomImgSectionProps> = ({ isMobile }) => {
+  return (
+    <BottomImgSectionWrapper>
+      <img
+        src={IMAGES_PATH.leftBottom}
+        alt="라년큘러스2"
+        className="leftImg"
+      ></img>
+      {!isMobile && (
+        <img
+          src={IMAGES_PATH.rightBottom}
+          alt="목련4"
+          className="rightImg"
+        ></img>
+      )}
+    </BottomImgSectionWrapper>
+  );
+};
 
 export default BottomImgSection;
 
 const BottomImgSectionWrapper = styled.div`
-position: relative;
-width: 100vw;
-height: auto;
+  position: relative;
+  width: 100vw;
+  height: auto;
 
-.leftImg {
+  .leftImg {
     position: absolute;
     bottom: 0;
     left: 0;
     animation: bottomSlideRightLeft 3s infinite alternate;
-}
+  }
 
-@keyframes bottomSlideRightLeft {
+  @keyframes bottomSlideRightLeft {
     0% {
-        transform: translateY(0);
+      transform: translateY(0);
     }
-    50%{
-        transform: translateX(-5%);
+    50% {
+      transform: translateX(-5%);
     }
     100% {
-        transform: translateY(3%);
-    }    
-}
+      transform: translateY(3%);
+    }
+  }
 
-.rightImg {
+  .rightImg {
     position: absolute;
     bottom: 0;
     right: 0;
     animation: bottomSlideLeftRight 3s infinite alternate;
-}
-@keyframes bottomSlideLeftRight {
+  }
+  @keyframes bottomSlideLeftRight {
     0% {
-        transform: translateX(0);
+      transform: translateX(0);
     }
-    50%{
-        transform: translateX(5%);
+    50% {
+      transform: translateX(5%);
     }
     100% {
-        transform: translateY(3%);
-    }  
-}
-`
+      transform: translateY(3%);
+    }
+  }
+`;
--- src/component/mainpage/MiddleImgSection.tsx
+++ src/component/mainpage/MiddleImgSection.tsx
@@ -1,57 +1,56 @@
-import styled from '@emotion/styled'
-import { IMAGES_PATH } from '../../config/mainapge/paths';
+import styled from "@emotion/styled";
+import { IMAGES_PATH } from "../../config/mainapge/paths";
 
 const MiddleImgSection = () => {
-    return(
-        <MiddleImgSectionWrapper>
-            <img src={IMAGES_PATH.leftMiddle}  alt="소현이 생일 축하 목련2" className="leftImg"></img>
-            <img src={IMAGES_PATH.rightMiddle} alt="소현이 생일 축하 목련3" className="rightImg"></img>       
-        </MiddleImgSectionWrapper>
-    )
-}
+  return (
+    <MiddleImgSectionWrapper>
+      <img src={IMAGES_PATH.leftMiddle} alt="목련2" className="leftImg"></img>
+      <img src={IMAGES_PATH.rightMiddle} alt="목련3" className="rightImg"></img>
+    </MiddleImgSectionWrapper>
+  );
+};
 export default MiddleImgSection;
 
-const MiddleImgSectionWrapper = styled.div` 
+const MiddleImgSectionWrapper = styled.div`
+  position: relative;
+  width: 100vw;
+  height: auto;
 
-position: relative;
-width: 100vw;
-height: auto;
-
-.leftImg {
+  .leftImg {
     position: absolute;
     top: 200px;
     left: 0;
     animation: middleSlideRightLeft 2s infinite alternate;
-}
+  }
 
-@keyframes middleSlideRightLeft {
+  @keyframes middleSlideRightLeft {
     0% {
-        transform: translateY(0);
+      transform: translateY(0);
     }
-    50%{
-        transform: translateY(3%);
+    50% {
+      transform: translateY(3%);
     }
     100% {
-        transform: translateX(-3%);
-    }    
-}
+      transform: translateX(-3%);
+    }
+  }
 
-.rightImg {
+  .rightImg {
     position: absolute;
     top: 200px;
     right: 0;
     animation: middleSlideLeftRight 2s infinite alternate;
-}
+  }
 
-@keyframes middleSlideLeftRight {
+  @keyframes middleSlideLeftRight {
     0% {
-        transform: translateX(0);
+      transform: translateX(0);
     }
-    50%{
-        transform: translateY(3%);
+    50% {
+      transform: translateY(3%);
     }
     100% {
-        transform: translateX(3%);
-    }  
-}
-`
+      transform: translateX(3%);
+    }
+  }
+`;
