c50eac358708584dfd51ba9110bb1b08bd2d1e03
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-12-16 10:56:34 +0900

    chore: sw.js 추가

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-restricted-globals */
+// public/sw.js
+// install service worker
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] installed");
+});
+
+// update service worker
+self.addEventListener("activate", (e) => {
+  console.log("[Service Worker] actived", e);
+});
+
+// fetch service worker
+self.addEventListener("fetch", (e) => {
+  console.log("[Service Worker] fetched resource " + e.request.url);
+});
